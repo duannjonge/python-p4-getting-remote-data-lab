4ffb261bc167510d53654417c697162e2f6862fd
duan spot <brandennjonge@gmail.com>
2023-09-28 15:40:23 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_content = self.get_response_body()
+        json_data = json.loads(response_content.decode('utf-8'))  # Decode bytes to a UTF-8 string
+        return json_data
+
+# Example usage:
+# get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+# print(get_requester.load_json())
